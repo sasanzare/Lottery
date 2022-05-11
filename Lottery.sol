3bb1b71e82e018d22a4690a750349f969b6fd627
sasanzare <sasanzare75@gmail.com>
2022-05-11 19:26:53 +0430

    Functions definition

--- Lottery.sol
+++ Lottery.sol
@@ -21,18 +21,36 @@ contract Lottery{
     event buyTicket(address indexed addr,uint256 amount,uint256 ticketCode);
     event winner(address indexed addr,uint256 amount,uint256 ticketCode);
     
-    constructor(uint16 _day){
-        day=_day;
-    }
-
-    function buyTicket() public payable returns(uint256){ 
-
-    }
+   	constructor(uint16 _day){
+		day=_day;
+		owner = msg.sender;
+		startDate = block.timestamp;
+	}
+	
+	function buyTicke() public payable returns(uint256){ 
+		require(msg.value == ticketPrice);
+		require(block.timestamp<startDate +(day*84600));
+		owner.transfer(msg.sender/10);
+        ticketCode++;
+        invested +=(msg.value*90)/100;
+        tickets[ticketCode]=Ticket(ticketCode,block.timestamp,msg.sender,false);
+        emit buyTicket(msg.sender,msg.value,ticketCode);
+        return ticketCode;
+        
+	}
 
     function startLottery() public{ 
+        require(msg.sender==owner);
+        require(block.timestamp<startDate +(day*84600));
+        require(isLotteryDone==false);
+        uint256 winnerIndex = random(ticketCode);
+        tickets[winnerIndex].win = true;
+        tickets[winnerIndex].member.transfer(invested);
+        isLotteryDone = true;
+        emit winner(tickets[winnerIndex].member,invested,winnerIndex);
 
     }
     function random(uint count) private view returns(uint){
-
+       return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty)))% count;
     }
 }
